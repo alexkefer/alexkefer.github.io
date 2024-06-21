2c76d7746cc5a505cb66b6680638b28b1a2d6cc1
alex kefer <69139463+alexkefer@users.noreply.github.com>
2024-06-21 04:07:22 -0700

    removed unused imports

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,35 +1,5 @@
 import { useEffect, useState } from "react";
 import Layout from "../Layout";
-import { IoLogoJavascript } from "react-icons/io";
-import {
-  SiAstro,
-  SiC,
-  SiCsharp,
-  SiExpress,
-  SiJest,
-  SiTailwindcss,
-  SiVite,
-  SiMongodb,
-} from "react-icons/si";
-import { TbBrandMysql } from "react-icons/tb";
-import { FaFilePdf, FaGolang } from "react-icons/fa6";
-import {
-  FaApple,
-  FaAws,
-  FaCss3Alt,
-  FaDocker,
-  FaFileDownload,
-  FaGitAlt,
-  FaGithub,
-  FaHtml5,
-  FaJava,
-  FaLinux,
-  FaNodeJs,
-  FaPython,
-  FaReact,
-  FaWindows,
-} from "react-icons/fa";
-import PDFViewer from "../components/PDFViewer.jsx";
 import AboutMe from "../components/AboutMe";
 import PDFSection from "../components/PDFSection.jsx";
 import Experience from "../components/Experience";
@@ -38,11 +8,6 @@ import Hobbies from "../components/Hobbies.jsx";
 import SkillsSidebar from "../components/SkillsSidebar.jsx";
 
 const About = () => {
-  const [isPDFOpen, setIsPDFOpen] = useState(false);
-
-  const togglePDF = () => {
-    setIsPDFOpen(!isPDFOpen);
-  };
   useEffect(() => {
     document.title = "Alex Kefer - About";
   }, []);
